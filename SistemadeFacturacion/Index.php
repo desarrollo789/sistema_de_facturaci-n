0477a42f21bfa17115299b84f6dc1bb8567d2168
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-04-01 14:46:21 -0600

    index finish

--- SistemadeFacturacion/Index.php
+++ SistemadeFacturacion/Index.php
@@ -16,8 +16,8 @@
     <link rel="stylesheet" href="style.css">    
 
     <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <script defer src="Resources\Awesome JS\solid.js"></script>
+    <script defer src="Resources\Awesome JS\fontawesome.js"></script>
 
     <!-- Our Custom JS -->
     <script defer src="anim.js"></script>
@@ -276,11 +276,11 @@
      <!-- jQuery CDN - Slim version (=without AJAX) -->
      <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
      <!-- Popper.JS -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+     <script src="Resources\Popper JS\popper.min.js"></script>
      <!-- Bootstrap JS -->
      <script src="Resources\Bootstrap JS\bootstrap.min.js"></script>
      <!-- jQuery Custom Scroller CDN -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+     <script src="Resources\jQuery Custom Scroller CDN\jquery.mCustomScrollbar.concat.min.js"></script>
      <!-- post logim request -->  
      <script src="helpers\POST.js"></script>        
 
