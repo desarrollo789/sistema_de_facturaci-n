f3c9db7ee9dc5df500ce52e77b2f16e35e1cc4cd
BrandonTrimer <44809226+BrandonTrimer@users.noreply.github.com>
2019-04-02 11:46:15 -0400

    estilos

--- SistemadeFacturacion/AdminUsuario.php
+++ SistemadeFacturacion/AdminUsuario.php
@@ -17,7 +17,7 @@
 	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
 	<script src="js/material.min.js" ></script>
 	<script src="js/sweetalert2.min.js" ></script>
-	<script src="js/jquery.mCustomScrollbar.concat.min.js" ></script>
+	<!--<script src="js/jquery.mCustomScrollbar.concat.min.js" ></script>-->
 	<script src="js/main.js" ></script>
 
     <!-- Bootstrap CSS CDN -->
